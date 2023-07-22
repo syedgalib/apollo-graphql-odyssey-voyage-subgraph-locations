981b2e04842115bbc7f7e17c0af81b96f1749692
Syed Galib Ahmed <syedgalibahmed@gmail.com>
2023-07-22 19:06:17 +0600

    adjustment for vercel

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import http from 'http';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import path from 'path';
 import { readFileSync } from 'fs';
 
 import { ApolloServer } from '@apollo/server';
@@ -9,9 +10,13 @@ import { expressMiddleware } from '@apollo/server/express4';
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
 import gql from 'graphql-tag';
 
-const typeDefs = gql( readFileSync( './api/locations.graphql', { encoding: 'utf8' } ) );
 
-console.log( { typeDefs } );
+// const locationsGraph = './api/locations.graphql';
+const locationsGraphPath = path.resolve( process.cwd(), 'api/locations.graphql' );
+
+const typeDefs = gql( readFileSync( locationsGraphPath, { encoding: 'utf8' } ) );
+
+console.log( { locationsGraphPath, typeDefs } );
 
 const app = express();
 
