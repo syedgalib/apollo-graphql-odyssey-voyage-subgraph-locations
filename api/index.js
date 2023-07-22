af82da92f40b65dda0f3195b47ef99f7a075b6ff
Syed Galib Ahmed <syedgalibahmed@gmail.com>
2023-07-22 17:38:30 +0600

    adjustment for vercel

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 import express from 'express';
 const app = express();
 
+const typeDefs = gql( await readFile('./api/locations.graphql', { encoding: 'utf-8' }) );
+
+console.log( { typeDefs } );
+
 app.get( '/', (req, res) => {
     res.setHeader('Content-Type', 'text/html');
     res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
